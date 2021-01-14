bd2c145e4bbad39169dc6722ad30fa5f6b514840
Emily Ryan <emilyryan@Emilys-MacBook-Pro.local>
2021-01-14 16:05:55 -0600

    prepared port for deployment to Heroku

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // require your server and start it
+require('dotenv').config()
 const server = require('./api/server');
 
-server.listen(4000, () => {
-    console.log('\n* Server Running on http://localhost:4000 *\n');
+const PORT = process.env.PORT || 5000
+
+console.log(process.env.NODE_ENV)
+
+server.listen(PORT, () => {
+    console.log(`\n* Server Running on port ${PORT} *\n`);
 })
